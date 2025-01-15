8c4e7da6bf7cf4ef5fa049c8182838ccc98c335f
Poojithachalla <poojitha22.ch@gmail.com>
2025-01-15 10:20:13 -0500

    First welcome msg

---
+++ src/main/java/com/example/Welcome/Test/WelcomeTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.Welcome.Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomeTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Welcome/Test/WelcomeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Welcome.Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelcomeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
